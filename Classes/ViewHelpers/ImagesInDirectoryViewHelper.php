4f424204ddc84b0d7ba613d90e79f0f82e3c543b
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2015-08-07 12:25:35 +0200

    [FEATURE] FM-85 Add random image for header

---
+++ Classes/ViewHelpers/ImagesInDirectoryViewHelper.php
@@ -0,0 +1,67 @@
+<?php
+namespace Subugoe\TmplFidmath\ViewHelpers;
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Ingo Pfennigstorf <pfennigstorf@sub-goettingen.de>
+ *      Goettingen State Library
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Random image from folder
+ */
+class ImagesInDirectoryViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * Initialize arguments.
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('directory', 'string', 'directory where the files reside', true);
+        $this->registerArgument('storageUid', 'int', 'FAL storage uid', false, 1);
+    }
+
+
+    public function render()
+    {
+
+        /** @var \TYPO3\CMS\Core\Resource\StorageRepository $storageRepository */
+        $storageRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Resource\StorageRepository::class
+        );
+
+        $storage = $storageRepository->findByUid($this->arguments['storageUid']);
+        $folder = $storage->getFolder($this->arguments['directory']);
+
+        $files = $folder->getFiles();
+        shuffle($files);
+
+        /** @var \TYPO3\CMS\Core\Resource\File $file */
+        $file = $files[0];
+
+       $this->templateVariableContainer->add('randomImage', $file);
+    }
+
+}
