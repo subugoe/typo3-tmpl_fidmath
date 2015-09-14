44c2cbd56b3debea4a6f2c6ecd02047cbf5e7744
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2015-09-14 15:07:44 +0200

    [FEATURE] Scaffold tx_find for fidmath

---
+++ Configuration/TypoScript/Elements/40_extension-find.ts
@@ -0,0 +1,41 @@
+plugin.tx_find.view.templateRootPaths.15 = EXT:tmpl_fidmath/Resources/Private/Templates/Extensions/Find/Templates/
+plugin.tx_find.view.partialRootPaths.15 = EXT:tmpl_fidmath/Resources/Private/Templates/Extensions/Find/Partials/
+plugin.tx_find.view.layoutRootPaths.15 = EXT:tmpl_fidmath/Resources/Private/Templates/Extensions/Find/Layouts/
+
+plugin.tx_find.settings {
+	connection {
+		host = solr
+		port = 8983
+		path = /solr/hansolr
+	}
+
+	standardFields {
+		title = title
+		snippet = content
+	}
+
+	CSSPaths {
+		10 >
+		20 >
+	}
+
+	jumpToID >
+}
+
+plugin.tx_find.settings {
+	defaultQuery = Berufe:Mathematiker
+
+	queryFields {
+		0 {
+			type = Text
+			query = %s Berufe:Mathematiker
+		}
+	}
+
+	sort {
+		1 {
+			id = default
+			sortCriteria = Titel_sort asc
+		}
+	}
+}
