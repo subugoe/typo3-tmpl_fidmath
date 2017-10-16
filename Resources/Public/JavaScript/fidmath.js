623f476e548c606fa3ca815c8a9669ec1c12b5a0
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-10-16 15:24:45 +0200

    Add back-to-top functionality
    Resolves: FM-141

--- Resources/Public/JavaScript/fidmath.js
+++ Resources/Public/JavaScript/fidmath.js
@@ -1,3 +1,16 @@
-(function($) {
+(function ($) {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.to-top').fadeIn();
+    } else {
+      $('.to-top').fadeOut();
+    }
+  });
 
+  $('.to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
 })(jQuery);
