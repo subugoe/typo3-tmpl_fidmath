d7a25c4c081baa8a3433ccdbb8cd90d60e55188e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-10-19 10:55:27 +0200

    Add alphabetical list for mathematicians

--- Resources/Public/JavaScript/fidmath.js
+++ Resources/Public/JavaScript/fidmath.js
@@ -16,8 +16,57 @@
 
 })(jQuery);
 
-$(document).ready(function() {
+$(document).ready(function () {
   if (document.getElementById('pz2-field-all')) {
     document.getElementById('header-search-form_input').value = document.getElementById('pz2-field-all').getAttribute('value');
   }
+
+  if (document.querySelector('.tx-zamn-pi1')) {
+    var position = document.querySelector('.tx-zamn-pi1 h4:nth-of-type(2)');
+
+    var personList = $('.tx-zamn-pi1 ol:nth-of-type(2)').children('li');
+    var alphabetList = document.createElement('div');
+    alphabetList.setAttribute('class', 'alphabet-list');
+
+    var doolist = [];
+    $.each(personList, function (index, value) {
+      var letter = $(value).children('a').text()[0];
+      var lister = {
+        'letter': letter,
+        'position': index
+      };
+
+      if (doolist.filter(function (e) {
+            return e.letter === letter
+          }).length === 0) {
+        doolist.push(lister);
+      }
+    });
+
+    doolist.forEach(function (a, b) {
+      var alink = document.createElement('a');
+      alink.setAttribute('href', '#' + a.position);
+      alink.setAttribute('class', 'alphabet-list-element -' + a.position);
+      var alinkvalue = document.createTextNode(a.letter);
+
+      alink.appendChild(alinkvalue);
+
+      alphabetList.appendChild(alink);
+    });
+
+    $(position).append(alphabetList);
+  }
+
+
+  $('.alphabet-list a').click(function (e) {
+    var targetClass = $(e.target).attr('href').replace('#', '');
+
+    var targetElement = $('.tx-zamn-pi1 ol:nth-of-type(2) li:eq(' + targetClass + ')');
+
+    $('html, body').animate({
+      scrollTop: $(targetElement).offset().top
+    }, 250);
+
+    return false;
+  });
 });
