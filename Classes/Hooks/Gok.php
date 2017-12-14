a98310773b115961cb9b8768b81d56e5a2f53591
Unknown <pamuk@sub.uni-goettingen.de>
2017-12-14 12:06:32 +0100

    Comments cleared

--- Classes/Hooks/Gok.php
+++ Classes/Hooks/Gok.php
@@ -19,73 +19,6 @@ class Gok
         $arguments = $element->arguments;
         $extkey = Utility::extKey;
 
-//        return $js.';console.log('.$element->objectID.')';
-
-//        $js .=  "
-//            $( document ).ready(function() {
-//
-//                var newButton = document.createElement('p');
-//                newButton.classList.add(\"checkButton\");
-//                var textnode = document.createTextNode(\"Auswahl...\");
-//                newButton.appendChild(textnode);
-//
-//                var list = document.getElementById(\"tx_nkwgok-734\");
-//                list.insertBefore(newButton, list.childNodes[0]);
-//
-//
-//                $(\"ul#ul-734-MSC\").hide();
-//
-//                $(\".checkButton\").click(function(){
-//                    $(\"#ul-734-MSC\").toggle();
-//                });
-//
-//                $(\".level-0\").click(function() {
-//                    var text = $(this).html();
-//                    $(\".checkButton\").html(text);
-//                    $(\"ul#ul-734-MSC\").hide();
-//                });
-//
-//                function getSelectedValue(id) {
-//                    return $(\"#\" + id).find(\"dt a span.value\").html();
-//                }
-//
-//                $(document).bind(\"click\", function(e) {
-//                    var \$clicked = $(e.target);
-//                    if (! \$clicked.parents().hasClass(\"gokContainer\"))
-//                        $(\"ul#ul-734-MSC\").hide();
-//                        console.log(\"hasClass gokContainer\");
-//                });
-//            });
-//  ############################################################################
-//        function makeIntoSelect (id) {
-//            console.log(id);
-//            var ulElement = document.getElementById(id);
-//            $(ulElement).each(function(){
-//                var list=$(this),
-//                        select=$(document.createElement("select"))
-//                            .insertBefore($(this))
-//                            .change(function(){window.open($(this).val())
-//            });
-//
-//            $(">li a span.GOKName", this).each(function(){
-//                var onclickfunction = this.parentNode.getAttribute("onclick");
-//                var GOKIDclass = $(this).prev().html();
-//                var option=$(document.createElement("option"))
-//                    .appendTo(select)
-//                    .val(this.parentNode.href)
-//                    .attr('id',GOKIDclass)
-//                    .html('<a href="' + this.parentNode.href + '"onclick="'+ onclickfunction + '"/>' + $(this).html() + '<a/>');
-//
-//                if($(this).attr("class") === "open"){
-//                    option.attr("selected","selected");
-//                }
-//            });
-//
-//            list.remove();
-//
-//            });
-//        }
-//        ";
         $js .= "
         var mutexSelect = false;
         var newSelectedItem;
@@ -120,12 +53,13 @@ class Gok
                         var GOKIDclass = $(this).prev().html();
                         var option=$(document.createElement(\"option\"))
                         .appendTo(select)
-                        .val(this.parentNode.href)
+                        .val($(this).html())
                         .attr('id',GOKIDclass)
                         .html('<a href=\"' + this.parentNode.href + '\"onclick=\''+onclickfunction+'\'/>'+ '<span class=\"plusMinus\">' + $(this).prev().prev().html() + ' </span>' + '<span class=\"GOKID\">' + GOKIDclass + ' </span>' + '<span class=\"GOKNAME\">' + $(this).html() +' </span>' + '<a/>');
                         
                 });
-            
+                
+                $('#'+newSelectedItem).val();
                 list.hide();
             });
             
